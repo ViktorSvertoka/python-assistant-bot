335b7c507f0eab813d660d60b5aee735bb5cb98f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-21 00:21:00 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="Lana assistant bot",
+    version="1.0.0",
+    description="Team project Lana assistant bot",
+    url="https://github.com/ViktorSvertoka/python-assistant-bot",
+    author="python-assistant-bot",
+    license="MIT",
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    entry_points={"console_scripts": ["hi_lana=main:main"]},
+)
